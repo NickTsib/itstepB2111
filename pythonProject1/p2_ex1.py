ad26b3e4ecaa8806522c4efeae94dc3bfbf79697
patronyss <olgznch@gmail.com>
2025-01-26 11:46:45 +0200

    Para2. Sim student

---
+++ pythonProject1/p2_ex1.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.height = 160
+        print('Hi!')
+
+
+vasya = Student('Vasya')
+print(vasya.height)
+print(vasya.name)
